cb54efe993117bae2ede713a440312f3cd24f950
Vcelist <44344629+Vcelist@users.noreply.github.com>
2019-10-28 20:09:37 +0300

    Update script.js

--- eth/script.js
+++ eth/script.js
@@ -1,6 +1,6 @@
 document.body.innerHTML += "<button value=\"\" class=\"btn\" onclick=\"addText();\">" + some + "</button>";
 document.body.innerHTML += "<button value=\"\" class=\"btn\" onclick=\"addTex();\">" + "+" + "</button>";
-document.body.innerHTML += "<button value=\"\" class=\"btn\" onclick=\"\">" + "-" + "</button>";
+document.body.innerHTML += "<button value=\"\" class=\"btn\" onclick=\"remStyle();\">" + "-" + "</button>";
 document.body.innerHTML += "<input type=\"text\" name=\"pole\" size=\"10\" maxlength=\"15\" value=\"123\" />";
 document.body.innerHTML += "<p><button value=\"\" class=\"btn\" onclick=\"addStyle();\">" + "addStyle" + "</button></p>";
 document.body.innerHTML += "<br>";
@@ -21,4 +21,8 @@ function addTex() {
 function addStyle() {
     document.body.innerHTML += "<style>.btn{width:200px; height:50px; margin-right:10px; border-radius:50px; border:none; background:#1D8BB8; color:white; font-size:16px;}</style>";
     document.body.style = "background:#D8E0E7; color:red; font-size:18px;"
+}
+function remStyle() {
+    document.body.innerHTML += "<style></style>";
+    document.body.style = ""
 }
